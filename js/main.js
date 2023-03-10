2042eebf729efd31b9159cb699b4e14b5607da70
PetrosJacob <119673069+PetrosJacob@users.noreply.github.com>
2023-03-10 14:18:07 +0100

    10/03/2023
    Har nu två låtar uppe i diskografi.
    Fixade så att man kan klicka för att flippa

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
-let layer  = document.querySelector(".fade-layer")
-layer.addEventListener("click",showMenu)
+let layer = document.querySelector(".fade-layer")
+layer.addEventListener("click", showMenu)
 
-let button  = document.querySelector(".menu-button")
-button.addEventListener("click",showMenu)
+let button = document.querySelector(".menu-button")
+button.addEventListener("click", showMenu)
 
 function showMenu() {
-    let menu = document.querySelector(".menu")
-    menu.classList.toggle("show")
-    
-    let layer = document.querySelector(".fade-layer")
-    layer.classList.toggle("visible")
-}   
+  let menu = document.querySelector(".menu")
+  menu.classList.toggle("show")
 
+  let layer = document.querySelector(".fade-layer")
+  layer.classList.toggle("visible")
+}
 
+let flipCards = document.querySelectorAll(".flip-card");
+flipCards.forEach(flipCard => {
+  flipCard.addEventListener("click", flipCardFunction);
+});
 
-
-function flipCard() {
-    let flipToFront = document.querySelector(".flip-card") 
-   flipToFront.classList.toggle("hejsan")
+function flipCardFunction() {
+  let flipCardInner = this.querySelector(".flip-card-inner");
+  flipCardInner.classList.toggle("flip-card-flipped");
 }
