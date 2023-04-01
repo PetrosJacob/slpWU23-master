eef3408d4c58b69f5c59defd4752a6ec7b8cf1bc
PetrosJacob <119673069+PetrosJacob@users.noreply.github.com>
2023-04-01 18:26:58 +0200

    Fredag vecka 13

--- js/main.js
+++ js/main.js
@@ -4,9 +4,14 @@ layer.addEventListener("click", showMenu)
 let button = document.querySelector(".menu-button")
 button.addEventListener("click", showMenu)
 
+
 function showMenu() {
   let menu = document.querySelector(".menu")
   menu.classList.toggle("show")
+
+  let funnyMenu = document.querySelector(".funnyMenu")
+  funnyMenu.classList.toggle("show")
+
 
   let layer = document.querySelector(".fade-layer")
   layer.classList.toggle("visible")
