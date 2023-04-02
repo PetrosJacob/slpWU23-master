8b55859e68fd67ea92bb10f43ed961d07614ffd1
PetrosJacob <119673069+PetrosJacob@users.noreply.github.com>
2023-04-02 13:17:29 +0200

    Bits and bobs, 02/04
    Skapade "biograf" sidan men text, bilder och video

--- js/main.js
+++ js/main.js
@@ -9,10 +9,6 @@ function showMenu() {
   let menu = document.querySelector(".menu")
   menu.classList.toggle("show")
 
-  let funnyMenu = document.querySelector(".funnyMenu")
-  funnyMenu.classList.toggle("show")
-
-
   let layer = document.querySelector(".fade-layer")
   layer.classList.toggle("visible")
 }
